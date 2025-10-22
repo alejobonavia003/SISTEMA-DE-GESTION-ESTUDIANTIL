8836ab0837b669d427878191a84b7f9b5a70c602
lauty23gf <lautygiraudo23@gmail.com>
2025-10-22 13:49:38 -0300

    se agregaron todas las clases para los modelos

--- código/src/main/java/com/is1/proyecto/models/carrera/Materia.java
+++ código/src/main/java/com/is1/proyecto/models/carrera/Materia.java
@@ -3,8 +3,6 @@ package com.is1.proyecto.models;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
-import com.is1.proyecto.models.persona.*;
-
 @Table("Materia")
 public class Materia extends Model {
     
