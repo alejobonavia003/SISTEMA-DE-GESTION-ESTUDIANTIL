81868c6ea7a22ac0cdcd9f9f07c2ca54cb21c80b
miladelfino <miladelfino17@gmail.com>
2025-10-22 13:26:45 -0300

    modelo de tabla Materia

---
+++ código/src/main/java/com/is1/proyecto/models/Materia.java
@@ -0,0 +1,39 @@
+package com.is1.proyecto.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+import com.is1.proyecto.models.persona.*;
+
+@Table("Materia")
+public class Materia extends Model {
+    
+    public Integer getCodigo() {
+        return getInteger("codigo");
+    }
+
+    public void setCodigo(Integer codigo) {
+        set("codigo", codigo);
+    }
+    
+    public String getName() {
+        return getString("name");
+    }
+
+    public void setName(String name) {
+        set("name", name);
+    }
+
+    public Integer getIdPlan() {
+        return getInteger("id_plan");
+    }
+
+    public void setIdPlan(Integer id_plan) {
+        set("id_plan", id_plan);
+    }
+
+
+
+
+
+}
